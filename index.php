5506c93d981007e68db6b2eaa6469922a222321f
geraldarko <gerald.darko@ashesi.edu.gh>
2023-04-19 21:05:07 +0000

    change

--- index.php
+++ index.php
@@ -21,22 +21,7 @@ if ($text == "") {
 	$response .= "\nWater level sensor";
 	$response .= "\nPress 0 to return to main menu";
 
-}else if ($text == "1*0"){
-	$text == "";
-	break;
 
-} else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-	$response = "CON Turn pump on/off \n";
-	$response .= "1. Turn ON \n";
-	$response .= "1. Turn OFF";
-
-}else if ($text == "2*1"){
-	$response = "END Pump will be turn on(this might take a few seconds)";
-}else if($text == "2*2"){
-	$response = "END Pump will be turn off(this might take a few seconds)";
-}
 
 
 }
