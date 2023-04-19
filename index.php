e472cf600ff891892bfb50373207492b0617fe5d
geraldarko <gerald.darko@ashesi.edu.gh>
2023-04-19 21:33:04 +0000

    changes

--- index.php
+++ index.php
@@ -34,11 +34,12 @@ if ($text == "") {
 // // 	$text == "";
 // // 	break;
 
-// }else if ($text == "2*1"){
-// 	$response = "END Pump will be turn on(this might take a few seconds)";
-// }else if($text == "2*2"){
-// 	$response = "END Pump will be turn off(this might take a few seconds)";
-// }
+}else if ($text == "2*1"){
+    $response = "END Pump will be turn on(this might take a few seconds)";
+
+}else if($text == "2*2"){
+ 	$response = "END Pump will be turn off(this might take a few seconds)";
+}
 
 }
 
