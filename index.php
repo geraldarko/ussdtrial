d0d917ca88f3cc35278ff298ee6b1577323b8ca8
geraldarko <gerald.darko@ashesi.edu.gh>
2023-04-19 23:26:24 +0000

    y

--- index.php
+++ index.php
@@ -19,20 +19,12 @@ if ($text == "") {
         $response .= "Temperature sensor:\n";
         $response .= "Soil moisture sensor:\n";
         $response .= "Water level sensor:\n";
-        $response .= "0. Back\n";
 	// $response .= "Press 0 to return to main menu\n";
 
 }   else if ($text == "2"){
         $response = "CON Turn pump on/off \n";
         $response .= "1. Turn ON \n";
         $response .= "2. Turn OFF \n";
-        $response .= "0. Back";
-
-} else if ($text == "2*0"){
-    // User selected option to go back to main menu
-        $response = "CON Welcome to Akuafo, what would you like to check? \n";
-        $response .= "1) Farm Sensor Readings \n";
-        $response .= "2) Irrigation Control";
     
 }   else if ($text == "2*1"){
         $response = "END Pump will be turn on(this might take a few seconds)";
