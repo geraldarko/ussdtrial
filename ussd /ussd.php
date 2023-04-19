c15cf2095269e426253c62658234fec016d4607e
geraldarko <gerald.darko@ashesi.edu.gh>
2023-04-19 13:33:12 +0000

    trial

--- ussd /ussd.php
+++ ussd /ussd.php
@@ -1,39 +1,37 @@
 <?php
 
-//reads variables sent via method post
-$sessionid = $_POST['SensorDateId'];
-$serviceCode = $_POST['serviceCode'];
-$text = $_POST['text'];
-$humidity = $_POST['humidity'];
-$temperature = $_POST['temperature'];
-$soilmoisture = $_POST['soilmoisture'];
-$waterlevel = $_POST['waterlevel'];
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
-if ($text == ""){
-	$response = "CON What would you want to check? \n";
-	$response .= "1. Farm Sensor Readings \n";
-	$response .= "2. Farm Pump Status";
-}else if ($text == "1"){
-	$response = "CON Farm Status\n";
-	$response .= "Humidity sensor".$humidity;
-	$response .= "\nTemperature sensor".$temperature;
-	$response .= "\nSoil moisture sensor".$soilmoisture;
-	$response .= "\nWater level sensor".$waterlevel;
-	$response .= "\nPress 0 to return to main menu";
-}else if ($text == "1*0"){
-	$text == "";
-	break;
-}else if ($text == "2"){
-	$response = "CON Turn pump on/off \n";
-	$response .= "1. Turn ON \n";
-	$response .= "1. Turn OFF";
-}else if ($text == "2*1"){
-	$response = "END Pump will be turn on(this might take a few seconds)";
-}else if($text == "2*2"){
-	$response = "END Pump will be turn off(this might take a few seconds)";
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
+
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
+
 }
 
+// Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
-
 ?>
