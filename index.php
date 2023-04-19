d2084be1cc9cddf5121afc165243c8dc4ae7f89b
geraldarko <gerald.darko@ashesi.edu.gh>
2023-04-19 21:29:13 +0000

    changes

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 // Read the variables sent via POST from our API
@@ -9,8 +11,8 @@ $text        = $_POST["text"];
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
 	$response = "CON Welcome to Akuafo, what would you like to check? \n";
-	$response .= "1. Farm Sensor Readings \n";
-	$response .= "2. Irrigation Control";
+	$response .= "1) Farm Sensor Readings \n";
+	$response .= "2) Irrigation Control";
     
 } else if ($text == "1") {
     // Business logic for first level response
@@ -19,18 +21,18 @@ if ($text == "") {
 	$response .= "Temperature sensor:\n";
 	$response .= "Soil moisture sensor:\n";
 	$response .= "Water level sensor:\n";
-	$response .= "Press 0 to return to main menu\n";
+	// $response .= "Press 0 to return to main menu\n";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
 	$response = "CON Turn pump on/off \n";
-	$response .= "1. Turn On \n";
-	$response .= "2. Turn Off";
+	$response .= "1) Turn On \n";
+	$response .= "2) Turn Off";
 
 // }else if ($text == "1*0"){
-// 	$text == "";
-// 	break;
+// // 	$text == "";
+// // 	break;
 
 // }else if ($text == "2*1"){
 // 	$response = "END Pump will be turn on(this might take a few seconds)";
