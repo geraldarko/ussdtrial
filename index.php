db4bbcea81335d293cccf837a480932616abfe90
geraldarko <gerald.darko@ashesi.edu.gh>
2023-04-19 22:53:46 +0000

    changes

--- index.php
+++ index.php
@@ -1,89 +1,79 @@
 <?php
 
-// Read the variables sent via POST from our USSD API
-$sessionId   = $_POST["sessionId"];
-$serviceCode = $_POST["serviceCode"];
-$phoneNumber = $_POST["phoneNumber"];
-$text        = $_POST["text"];
+// require_once 'db_cred.php';
 
-// Split the text input by '*' to get an array of user input options
-$inputOptions = explode('*', $text);
+// // Create connection
+// $conn = new mysqli($SERVER, $USERNAME, $PASSWD, $DATABASE);
 
-// Get the last input option from the array
-$lastInputOption = end($inputOptions);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
-// Determine the current level of the USSD menu based on the count of input options
-$currentLevel = count($inputOptions);
+// // Your PHP code here for executing database queries
 
-// Initialize response
-$response = "";
+// // Close connection
+// $conn->close();
 
-// Check if user input is empty
+
+// Read the variables sent via POST from our API
+    $sessionId   = $_POST["sessionId"];
+    $serviceCode = $_POST["serviceCode"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $text        = $_POST["text"];
+
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response = "CON Welcome to Akuafo, what would you like to check? \n";
-    $response .= "1) Farm Sensor Readings \n";
-    $response .= "2) Irrigation Control";
+        $response = "CON Welcome to Akuafo, what would you like to check? \n";
+        $response .= "1) Farm Sensor Readings \n";
+        $response .= "2) Irrigation Control";
+        
+} else if ($text == "1") {
+    // Business logic for first level response
+        $response = "CON Your Farm Readings are\n";
+        $response .= "Humidity sensor:\n";
+        $response .= "Temperature sensor:\n";
+        $response .= "Soil moisture sensor:\n";
+        $response .= "Water level sensor:\n";
+        $response .= "0. Back\n";
+	// $response .= "Press 0 to return to main menu\n";
+
+}   else if ($text == "2"){
+        $response = "CON Turn pump on/off \n";
+        $response .= "1. Turn ON \n";
+        $response .= "2. Turn OFF";
+        $response .= "0. Back";
+
+} else if ($text == "1*0"){
+    // User selected option to go back to main menu
+        $response = "CON Welcome to Akuafo, what would you like to check? \n";
+        $response .= "1) Farm Sensor Readings \n";
+        $response .= "2) Irrigation Control";
+
+} else if ($text == "2*0"){
+    // User selected option to go back to main menu
+        $response = "CON Welcome to Akuafo, what would you like to check? \n";
+        $response .= "1) Farm Sensor Readings \n";
+        $response .= "2) Irrigation Control";
     
-} else {
-    // Check the current level of the USSD menu and provide appropriate response
-    switch ($currentLevel) {
-        case 1:
-            // Level 1 response
-            switch ($lastInputOption) {
-                case 1:
-                    // Business logic for first level response - Farm Sensor Readings
-                    $response = "CON Your Farm Readings are\n";
-                    $response .= "Humidity sensor:\n";
-                    $response .= "Temperature sensor:\n";
-                    $response .= "Soil moisture sensor:\n";
-                    $response .= "Water level sensor:\n";
-                    $response .= "Press 0 to return to main menu\n";
-                    break;
-                case 2:
-                    // Business logic for first level response - Irrigation Control
-                    $response = "CON Turn pump on/off \n";
-                    $response .= "1) Turn On \n";
-                    $response .= "2) Turn Off";
-                    break;
-                default:
-                    // Invalid input, display error message
-                    $response = "CON Invalid input. Press 0 to return to main menu";
-                    break;
-            }
-            break;
-        case 2:
-            // Level 2 response
-            switch ($lastInputOption) {
-                case 0:
-                    // User selected option to go back to main menu
-                    $response = "CON Welcome to Akuafo, what would you like to check? \n";
-                    $response .= "1) Farm Sensor Readings \n";
-                    $response .= "2) Irrigation Control";
-                    break;
-                case 1:
-                    // Business logic for turning the pump on
-                    $response = "END Pump will be turned on (this might take a few seconds)";
-                    break;
-                case 2:
-                    // Business logic for turning the pump off
-                    $response = "END Pump will be turned off (this might take a few seconds)";
-                    break;
-                default:
-                    // Invalid input, display error message
-                    $response = "CON Invalid input. Press 0 to return to main menu";
-                    break;
-            }
-            break;
-        default:
-            // Invalid input, display error message
-            $response = "CON Invalid input. Press 0 to return to main menu";
-            break;
-    }
+
+} else if ($text == "1*0*1"){
+        $response = "CON Your Farm Readings are\n";
+        $response .= "Humidity sensor:\n";
+        $response .= "Temperature sensor:\n";
+        $response .= "Soil moisture sensor:\n";
+        $response .= "Water level sensor:\n";
+        $response .= "0. Back\n";
+
+}   else if ($text == "2*1"){
+        $response = "CON Pump will be turn on(this might take a few seconds)";
+
+}   else if($text == "2*2"){
+        $response = "CON Pump will be turn off(this might take a few seconds)";
+
 }
 
-// Echo the response back to the USSD API
+// Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
-
 ?>
